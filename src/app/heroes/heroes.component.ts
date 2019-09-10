4f5d05563f8552e232d41cc0355791930bdb8246
isancosmed <isancosmed@itg.es>
2019-09-10 10:34:26 +0200

    5.routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +9,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
 
   constructor(private heroService: HeroService) { }
 
@@ -25,4 +16,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
